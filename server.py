83ae051b50d0060ef28b895929cae96d2e400531
muhammadibi7860 <muhammadibi7860@gmail.com>
2025-12-19 22:04:39 +0500

    Revert "Merge pull request #2 from muhammadibi7860-bot/feature2"
    This reverts commit ff21efb50dd5fe4494bfb3644c0e7c5c7ab23382, reversing
    changes made to fa45f8fd6c435c915ad0a2cd124fd2c46ea3d965.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
 
-    return "Welcome to Alphabridge this one is for testing only"
+    return "Welcome to Alphabridge feature 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
