1696570eea79da32a7125cca23c6ac28ce8959fa
muhammadibi7860 <muhammadibi7860@gmail.com>
2025-12-19 20:29:34 +0500

    c2

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to Alphabridge"
+    return "Welcome to Alphabridge feature 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
